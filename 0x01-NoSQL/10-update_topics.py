79900c88b7576b8fd8b6140a9806ee2159b5cbc2
DYung26 <theadstore1407@gmail.com>
2024-10-21 18:25:14 -0700

    `Change school topics` - Add module for updating school topics in a MongoDB collection with appropriate docstrings.

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Module to interact with MongoDB collections for school documents.
+
+This module provides functions to update school documents in a MongoDB
+collection, including updating the topics associated with a specific school
+name.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update the topics of a school document in a MongoDB collection.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The MongoDB
+        collection to update.
+        name (str): The name of the school document to update.
+        topics (list): The list of topics to set for the school document.
+
+    Returns:
+        UpdateResult: The result of the update operation.
+    """
+    mongo_collection.update(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
