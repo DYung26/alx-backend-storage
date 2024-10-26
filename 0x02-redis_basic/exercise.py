573aac4e64503f87cfe3016aa09749e7046c9053
DYung26 <theadstore1407@gmail.com>
2024-10-25 17:34:50 -0700

    Add `get`, `get_str`, and `get_int` methods to `Cache` class for data retrieval

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 """
 This module provides a Cache class to interface with Redis,
 allowing storage of data with auto-generated unique keys.
+It includes methods for storing and retrieving various data types.
 """
 
 import redis
@@ -30,3 +31,43 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key, fn):
+        """
+        Retrieve data from Redis by key, applying an optional transformation
+        function.
+
+        Parameters:
+        - key (str): The key under which the data is stored.
+        - fn (Optional[Callable[[bytes], Union[str, int]]]): A function to
+        transform the retrieved data.
+
+        Returns:
+        - Optional[Union[str, int]]: The retrieved data after transformation,
+        or None if not found.
+        """
+        return fn(self._redis.get(key)) if fn is not None else self._redis.get(key)
+
+    def get_str(key):
+        """
+        Retrieve a string value from Redis by key.
+
+        Parameters:
+        - key (str): The key under which the string is stored.
+
+        Returns:
+        - Optional[str]: The decoded string value, or None if not found.
+        """
+        self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(key):
+        """
+        Retrieve an integer value from Redis by key.
+
+        Parameters:
+        - key (str): The key under which the integer is stored.
+
+        Returns:
+        - Optional[int]: The integer value, or None if not found.
+        """
+        self.get(key, int)
